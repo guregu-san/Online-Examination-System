b17d1cfc29a22ed6fd6b493a81832353daff1c97
Greg <206912153+guregu-san@users.noreply.github.com>
2025-11-11 20:24:18 +0200

    updated schema

--- src/backend/initializeDB.sql
+++ src/backend/initializeDB.sql
@@ -1,22 +1,29 @@
-CREATE TABLE instructors (
+CREATE TABLE IF NOT EXISTS instructors (
     instructor_id INTEGER PRIMARY KEY,
     name TEXT,
     email TEXT,
     password_hash TEXT
 );
 
-CREATE TABLE courses (
-    course_id INTEGER PRIMARY KEY,
-    course_code TEXT,
-    course_name TEXT,
-    instructor_id INTEGER,
-    FOREIGN KEY (instructor_id) REFERENCES instructors(instructor_id)
+CREATE TABLE IF NOT EXISTS students (
+    roll_number INTEGER PRIMARY KEY,
+    name TEXT,
+    email TEXT UNIQUE,
+    password TEXT,
+    contact_number INTEGER
 );
 
-CREATE TABLE exams (
+CREATE TABLE IF NOT EXISTS courses (
+    course_code TEXT PRIMARY KEY,
+    course_name TEXT NOT NULL,
+    instructor_email TEXT NOT NULL,
+    FOREIGN KEY (instructor_email) REFERENCES instructors(email)
+);
+
+CREATE TABLE IF NOT EXISTS exams (
     exam_id INTEGER PRIMARY KEY,
     course_id INTEGER,
-    instructor_id INTEGER,
+    instructor_email TEXT,
     title TEXT,
     time_limit INTEGER,
     security_settings TEXT,
@@ -26,19 +33,35 @@ CREATE TABLE exams (
     FOREIGN KEY (instructor_id) REFERENCES instructors(instructor_id)
 );
 
-CREATE TABLE questions (
+CREATE TABLE IF NOT EXISTS questions (
     question_id INTEGER PRIMARY KEY,
-    exam_id INTEGER,
-    question_text TEXT,
-    question_type TEXT CHECK(question_type IN ('MCQ_SINGLE', 'MCQ_MULTIPLE', 'TRUE_FALSE', 'SHORT_ANSWER', 'NUMERICAL', 'ESSAY')),
-    order_index INTEGER,
+    exam_id INTEGER NOT NULL,
+    question_text TEXT NOT NULL,
+    is_multiple_correct BOOLEAN NOT NULL,
+    points INTEGER NOT NULL,
+    order_index INTEGER NOT NULL,
     FOREIGN KEY (exam_id) REFERENCES exams(exam_id)
 );
 
-CREATE TABLE answers (
-    answer_id INTEGER PRIMARY KEY,
-    question_id INTEGER,
-    answer_text TEXT,
-    is_correct BOOLEAN,
+CREATE TABLE IF NOT EXISTS options (
+    option_id INTEGER PRIMARY KEY,
+    question_id INTEGER NOT NULL,
+    option_text TEXT NOT NULL,
+    is_correct BOOLEAN NOT NULL,
     FOREIGN KEY (question_id) REFERENCES questions(question_id)
+);
+
+CREATE TABLE IF NOT EXISTS submissions (
+    submission_id INTEGER PRIMARY KEY,
+    exam_id INTEGER NOT NULL,
+    roll_number TEXT NOT NULL,
+    started_at DATETIME,
+    submitted_at DATETIME,
+    updated_at DATETIME,
+    feedback TEXT,
+    status TEXT CHECK (status IN ('IN_PROGRESS', 'SUBMITTED', 'IN_REVIEW', 'GRADED')),
+    answers TEXT,
+    total_score INTEGER,
+    FOREIGN KEY (exam_id) REFERENCES exams (exam_id),
+    FOREIGN KEY (student_email) REFERENCES students (email)
 );
